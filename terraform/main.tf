afd334741eb8d1e1bed17ed129969dcf4675b185
froOst23 <frost.dat@gmail.com>
2020-04-05 15:29:22 +0500

    minor changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,16 +6,31 @@ provider "heroku" {
   api_key = "${var.heroku_api_key}"
 }
 
-# Configure assistance-bot-tf app
-resource "heroku_app" "develop" {
-  name = "assistance-bot-tf"
+# Create Heroku apps for staging and production
+resource "heroku_app" "staging" {
+  name = "assistance-bot-staging"
   region = "eu"
-  sensitive_config_vars = "${var.sensitive_config_vars}"
+  sensitive_config_vars = "${var.sensitive_config_vars_staging}"
 }
 
-# Configure build
-resource "heroku_build" "develop" {
-  app = "${heroku_app.develop.name}"
+resource "heroku_app" "production" {
+  name = "assistance-bot-production"
+  region = "eu"
+  sensitive_config_vars = "${var.sensitive_config_vars_production}"
+}
+
+# Configure build apps for staging and production
+resource "heroku_build" "staging" {
+  app = "${heroku_app.staging.name}"
+  buildpacks = ["https://github.com/heroku/heroku-buildpack-python"]
+  source = {
+    url = "https://github.com/froOst23/assistance_bot/archive/v1.0.tar.gz"
+    version = "v1.0"
+  }
+}
+
+resource "heroku_build" "production" {
+  app = "${heroku_app.production.name}"
   buildpacks = ["https://github.com/heroku/heroku-buildpack-python"]
   source = {
     url = "https://github.com/froOst23/assistance_bot/archive/v1.0.tar.gz"
